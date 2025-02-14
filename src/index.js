63eae4d28f432a19fbf860f496a56cab277d8174
Aryman Rawat <aryman1.rawat@paytmmoney.com>
2025-02-14 17:18:46 +0530

    body-parser setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const { PORT } = require("./config/config");
+const bodyParser = require("body-parser")
+
+
 
 const setupAndStartServer = () => {
 
     const app = express();
+
+    app.use(bodyParser);
+    app.use(bodyParser.urlencoded({extended : true}));
 
     app.listen(3000, () => {
         console.log(`Server is listening at ${PORT}`);
