9dc9100fca6b2e354cef43206e310d4648cf4aa2
Aryman Rawat <aryman1.rawat@paytmmoney.com>
2025-03-02 21:30:11 +0530

    db: created airports table

--- src/models/airport.js
+++ src/models/airport.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,16 +11,19 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.City.hasMany(models.Airport, {
-        foreignKey : 'cityId',
+      models.Airport.belongsTo(models.City, {
+        foreignKey: 'cityId',
+        onDelete: 'CASCADE'
       });
     }
   }
-  City.init({
-    name: DataTypes.STRING
+  Airport.init({
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    cityId: DataTypes.NUMBER
   }, {
     sequelize,
-    modelName: 'City',
+    modelName: 'Airport',
   });
-  return City;
+  return Airport;
 };
